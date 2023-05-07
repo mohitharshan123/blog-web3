a25a1d85ae56e4d332652d9c3331c57db59009bb
Mohit <mohitharshan.mec@gmail.com>
2023-05-07 12:26:35 +0530

    Initial commit from npx thirdweb create

---
+++ client/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/mohitharshan/Desktop/thirdweb/client/app/layout.tsx": [
+      "/Users/mohitharshan/Desktop/thirdweb/client/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/mohitharshan/Desktop/thirdweb/client/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/mohitharshan/Desktop/thirdweb/client/app/page": [
+      "/Users/mohitharshan/Desktop/thirdweb/client/app/globals.css",
+      "/Users/mohitharshan/Desktop/thirdweb/client/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
